12766b981e6fecf8f99f7d230a5fb0fe89b82c80
Oleksandr Kononenko <s_com@ukr.net>
2019-06-27 22:37:06 +0300

    first modal

---
+++ modals/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+
+      <section>
+        <div class="container">
+          <button class="btn btnPrimary" @click="modalFirst=!modalFirst">Show first modal</button>
+          <!-- first modal -->
+          <modals 
+          title="First modal"
+          v-if="modalFirst"
+          >
+
+          </modals>
+        </div>
+      </section>
+
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+import modals from '@/components/Modal.vue'
+
+export default { 
+  name: 'modals app',
+  components: {
+    modals
+  },
+  data() {
+    return {
+      modalFirst:false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modals/vue.config.js
@@ -0,0 +1,20 @@
+// vue.config.js
+
+// site.com/modals
+
+// site.com/css/main.css
+// site.com/modals/css/main.css
+
+module.exports = {
+
+    publicPath: process.env.NODE_ENV === 'production'
+    // на серевер только с /
+
+    ? ''
+    // ? '/modals'
+    
+    : '/',
+    assetsDir: '',
+    // assetsDir: 'assets',
+    productionSourceMap: false
+}
