d10ebff421b4bcee18ab54ce0ade86a9dbeba46f
Oleksandr Kononenko <s_com@ukr.net>
2019-07-01 17:06:01 +0300

    modalValidate, Vuelidate

--- modals/src/components/ModalValidate.vue
+++ modals/src/components/ModalValidate.vue
@@ -1,17 +1,29 @@
 <template>
      <modal 
-            title="Modal with form"
+            title="Modal with form + Validate"
             @close="$emit('close')"
           >
             <!-- use <form> <ul> or other tag with slot name-->
             <div slot="body">
                   <form @submit.prevent="">
-                    <label>Name:
-                      <input type="text" required >
-                    </label>
+
+                    <div class="form-item" :class="{ errorInput: $v.name.$error }">
+                        <label>Name:</label>
+                          <p class="errorText" v-if="!$v.name.required">Filed is required!</p>
+                          <p class="errorText" v-if="!$v.name.minLength">Name must have at least {{$v.name.$params.minLength.min}} !</p>
+                          <input 
+                           v-model="name"
+                           :class="{ error: $v.name.$error }"
+                           @change="$v.name.$touch()"
+                           >
+                     </div>
+
+                    
                     <label>Email:
-                      <input type="email" required >
+                      <input  v-modal="email" >
                     </label>
+                    <!-- <div>{{email}}</div> -->
+                    
                     <button class="btn btnPrimary">Submit</button>
                   </form>
     
@@ -25,10 +37,51 @@
 </template>
 
 <script>
+import { required, minLength, email } from 'vuelidate/lib/validators'
+
 import modal from '@/components/UI/Modal.vue'
+
+
 export default {
     components :{
         modal
+    },
+    data() {
+        return {
+            name:'',
+            email: ''
+        }
+    },
+    validations: {
+    name: {
+      required,
+      minLength: minLength(4)
+    },
+    email: {
+      required,
+      email
     }
+  }
 }
 </script>
+
+<style lang="scss">
+
+.form-item .errorText {
+  display:none;
+  margin-bottom: 8px;
+  font-size: 14px;
+  color: rgb(190, 91, 91);
+}
+.form-item {
+  &.errorInput .errorText {
+  display:block;
+}
+}
+
+
+input.error {
+  border-color: rgb(190, 91, 91)
+}
+
+</style>
