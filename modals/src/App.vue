9ec4847355aceb48e459ebe342e1297511203d91
Oleksandr Kononenko <s_com@ukr.net>
2019-07-03 21:50:13 +0300

    clear hw

--- modals/src/App.vue
+++ modals/src/App.vue
@@ -64,7 +64,6 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 
 import modals from '@/components/UI/Modal.vue'
 import modalValidate from '@/components/ModalValidate.vue'
@@ -119,14 +118,6 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale; */
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
 .main {
     display:flex;
