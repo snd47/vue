d2ad45fc536c37527a14df3adbd171a7e66f3361
Oleksandr Kononenko <s_com@ukr.net>
2019-07-09 21:16:12 +0300

    vue-cli template

---
+++ vue-cli-router/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <div id="app">
+        <h1>Template vue-cli</h1>
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default { 
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
