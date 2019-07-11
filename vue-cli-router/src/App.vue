cebe0d3a1c90385b32ab82c70f7b6d3b6e6af50c
Oleksandr Kononenko <s_com@ukr.net>
2019-07-11 22:07:03 +0300

    router-link with linls array, v-for, bind; lodash upgr;hw del;

--- vue-cli-router/src/App.vue
+++ vue-cli-router/src/App.vue
@@ -9,12 +9,11 @@
             <div class="logo">Vue-cli</div>
 
             <ul class="navbar-list">
-              <li class="navbar-item">
-                 <router-link class="navbar-link" to="/">Home</router-link>
+              
+              <li class="navbar-item" v-for="link in links" :key="link.title">
+                 <router-link class="navbar-link" :title="link.title" :to="link.url">{{link.title}}</router-link>
               </li>
-              <li class="navbar-item">
-                 <router-link class="navbar-link" to="/example">Example</router-link>
-              </li>
+             
             </ul>
 
           </div>
@@ -30,12 +29,22 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default { 
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      links: [
+        {
+          title:'Home',
+          url: '/'
+        },
+        {
+          title:'Example',
+          url: '/example'
+        }
+      ]
+    }
   }
 }
 </script>
