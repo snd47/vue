e05043216bdb65a1f82b11ec5e3419afcbd3d221
Oleksandr Kononenko <s_com@ukr.net>
2019-07-12 20:25:50 +0300

    router-link-exact-active

--- vue-cli-router/src/App.vue
+++ vue-cli-router/src/App.vue
@@ -49,7 +49,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -57,5 +57,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.navbar-link {
+  &.router-link-exact-active {
+    color:blue;
+  }
 }
 </style>
