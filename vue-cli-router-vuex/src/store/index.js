b8833aa52e58449cc82c0b0caf5836968988ee4d
Oleksandr Kononenko <s_com@ukr.net>
2019-07-14 18:47:23 +0300

    state, mutations, actions, getters

--- vue-cli-router-vuex/src/store/index.js
+++ vue-cli-router-vuex/src/store/index.js
@@ -4,8 +4,27 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions:{},
-    getters: {}
+    state: {
+        message: 'Hello Vuex'
+    },
+    // mutations(setter) for change state 
+    mutations: {
+        setMessage(state, payload) {
+            state.message = payload
+        }
+    },
+    // actions(setter) for async data
+    actions:{
+        setMessage({commit}) {
+            let mes = API
+            let mes2 = getters.getMessage()
+            commit('setMessage', {mes, mes2})
+        }
+    },
+    // bring data from state with getters
+    getters: {
+        getMessage(state) {
+            return state.message
+        }
+    }
 })
