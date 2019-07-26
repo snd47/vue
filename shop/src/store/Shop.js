066d8ab308b99e34ee7a83f8df57ca423b0ad440
Oleksandr Kononenko <s_com@ukr.net>
2019-07-26 22:14:13 +0300

    Shop.js gallery jpeg; 2 jpeg mistake names change

--- shop/src/store/Shop.js
+++ shop/src/store/Shop.js
@@ -6,49 +6,90 @@ export default {
                 id:1,
                 title: 'Fila Red',
                 descr: 'Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ac felis donec et odio pellentesque diam volutpat commodo sed. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Varius morbi enim nunc faucibus a pellentesque sit amet. Eu facilisis sed odio morbi quis commodo odio aenean. Vitae et leo duis ut diam quam nulla. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed.',
-                img: require('../assets/img/fila_webr1ps.png')
+                img: require('../assets/img/fila_webr1ps.png'),
+                gallery: [
+                    {name:'Fila red first', img: require('../assets/img/fila_webr1ps.png')},
+                    {name:'Fila red second', img: require('../assets/img/fila_webr2.jpeg')},
+                    {name:'Fila red third', img: require('../assets/img/fila_webr3.jpeg')}
+                ]
             },
             {
                 id:2,
                 title: 'Fila light',
                 descr: 'Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ac felis donec et odio pellentesque diam volutpat commodo sed. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Varius morbi enim nunc faucibus a pellentesque sit amet. Eu facilisis sed odio morbi quis commodo odio aenean. Vitae et leo duis ut diam quam nulla. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed.',
-                img: require('../assets/img/fila_megalite1.jpeg')
+                img: require('../assets/img/fila_megalite1.jpeg'),
+                gallery: [
+                    {name:'Fila light first', img: require('../assets/img/fila_megalite1.jpeg')},
+                    {name:'Fila light first', img: require('../assets/img/fila_megalite2.jpeg')},
+                    {name:'Fila light first', img: require('../assets/img/fila_megalite3.jpeg')}
+                ]
             },
             {
                 id:3,
                 title: 'Adidas',
                 descr: 'Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ac felis donec et odio pellentesque diam volutpat commodo sed. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Varius morbi enim nunc faucibus a pellentesque sit amet. Eu facilisis sed odio morbi quis commodo odio aenean. Vitae et leo duis ut diam quam nulla. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed.',
-                img: require('../assets/img/adidas_uboost1.png')
+                img: require('../assets/img/adidas_uboost1.png'),
+                gallery: [
+                    {name:'Adidas first', img: require('../assets/img/adidas_uboost1.png')},
+                    {name:'Adidas second', img: require('../assets/img/adidas_uboost2.jpeg')},
+                    {name:'Adidas third', img: require('../assets/img/adidas_uboost3.jpeg')},
+                    {name:'Adidas 4', img: require('../assets/img/adidas_uboost4.jpeg')}
+                ]
             },
             {
                 id:4,
                 title: 'Nike black',
                 descr: 'Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ac felis donec et odio pellentesque diam volutpat commodo sed. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Varius morbi enim nunc faucibus a pellentesque sit amet. Eu facilisis sed odio morbi quis commodo odio aenean. Vitae et leo duis ut diam quam nulla. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed.',
-                img: require('../assets/img/nike_black_rev1.jpeg')
+                img: require('../assets/img/nike_black_rev1.jpeg'),
+                gallery: [
+                    {name:'Nike black first', img: require('../assets/img/nike_black_rev1.jpeg')},
+                    {name:'Nike black second', img: require('../assets/img/nike_black_rev2.jpeg')},
+                    {name:'Nike black third', img: require('../assets/img/nike_black_rev3.jpeg')}
+                ]
             },
             {
                 id:5,
                 title: 'Nike white',
                 descr: 'Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ac felis donec et odio pellentesque diam volutpat commodo sed. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Varius morbi enim nunc faucibus a pellentesque sit amet. Eu facilisis sed odio morbi quis commodo odio aenean. Vitae et leo duis ut diam quam nulla. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed.',
-                img: require('../assets/img/nike_rev1wite.jpeg')
+                img: require('../assets/img/nike_rev1white.jpeg'),
+                gallery: [
+                    {name:'Nike white first', img: require('../assets/img/nike_rev1white.jpeg')},
+                    {name:'Nike white second', img: require('../assets/img/nike_rev2white.jpeg')},
+                    {name:'Nike white third', img: require('../assets/img/nike_rev3white.jpeg')}
+                ]
             },
             {
                 id:6,
                 title: 'Puma comete',
                 descr: 'Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ac felis donec et odio pellentesque diam volutpat commodo sed. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Varius morbi enim nunc faucibus a pellentesque sit amet. Eu facilisis sed odio morbi quis commodo odio aenean. Vitae et leo duis ut diam quam nulla. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed.',
-                img: require('../assets/img/puma_comete1.jpeg')
+                img: require('../assets/img/puma_comete1.jpeg'),
+                gallery: [
+                    {name:'Puma comete first', img: require('../assets/img/puma_comete1.jpeg')},
+                    {name:'Puma comete second', img: require('../assets/img/puma_comete2.jpeg')},
+                    {name:'Puma comete third', img: require('../assets/img/puma_comete3.jpeg')}
+                ]
             },
             {
                 id:7,
                 title: 'Puma hybrid',
                 descr: 'Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ac felis donec et odio pellentesque diam volutpat commodo sed. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Varius morbi enim nunc faucibus a pellentesque sit amet. Eu facilisis sed odio morbi quis commodo odio aenean. Vitae et leo duis ut diam quam nulla. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed.',
-                img: require('../assets/img/puma_hybc1.jpeg')
+                img: require('../assets/img/puma_hybc1.jpeg'),
+                gallery: [
+                    {name:'Puma hybrid first', img: require('../assets/img/puma_hybc1.jpeg')},
+                    {name:'Puma hybrid second', img: require('../assets/img/puma_hybc2.jpeg')},
+                    {name:'Puma hybrid third', img: require('../assets/img/puma_hybc3.jpeg')}
+                ]
             },
             {
                 id:8,
                 title: 'Skechers overhall',
                 descr: 'Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ac felis donec et odio pellentesque diam volutpat commodo sed. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Varius morbi enim nunc faucibus a pellentesque sit amet. Eu facilisis sed odio morbi quis commodo odio aenean. Vitae et leo duis ut diam quam nulla. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed.',
-                img: require('../assets/img/skech_overhall1.jpeg')
+                img: require('../assets/img/skech_overhall1.jpeg'),
+                gallery: [
+                    {name:'Skechers overhall first', img: require('../assets/img/skech_overhall1.jpeg')},
+                    {name:'Skechers overhall second', img: require('../assets/img/skech_overhall1.jpeg')},
+                    {name:'Skechers overhall third', img: require('../assets/img/skech_overhall1.jpeg')}
+                ]
             }
         ]
     },
